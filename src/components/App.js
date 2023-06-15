c407016dff18784f332fc07dbaf2507312a2b7eb
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-06-15 19:18:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  let [name, changeName] = useState("")
+  function changeHandle(event){
+      name = event.target.value
+      changeName(name)
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>Enter your name:</div>
+        <input type="text" onChange={changeHandle}></input>
+        <div>Hello {name}!</div>
     </div>
   )
 }
